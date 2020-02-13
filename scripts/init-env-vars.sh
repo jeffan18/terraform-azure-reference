39e8a423799f023aab451b825469451d4364cfed
jfan <46353840+jeffan18@users.noreply.github.com>
2020-02-13 15:10:04 -0500

    Update init-env-vars.sh

--- scripts/init-env-vars.sh
+++ scripts/init-env-vars.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-export LOCATION=westeurope
-export COMMON_RESOURCE_GROUP_NAME=terraform-ref-rg
-export TF_STATE_STORAGE_ACCOUNT_NAME=tfstate2195
-export TF_STATE_CONTAINER_NAME=tfstate-ref
-export KEYVAULT_NAME=terraform-ref-kv
+export LOCATION=eastus
+export COMMON_RESOURCE_GROUP_NAME=RG-Common-Resources
+export TF_STATE_STORAGE_ACCOUNT_NAME=forterraformbackend
+export TF_STATE_CONTAINER_NAME=tfstate
+export KEYVAULT_NAME=Key-Vault-fan-eastus
