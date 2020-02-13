2c6cda9c4f709fd6290ac522b7c6dd5b81150ebd
jfan <46353840+jeffan18@users.noreply.github.com>
2020-02-13 15:13:03 -0500

    Update init.sh

--- tf/common/init.sh
+++ tf/common/init.sh
@@ -7,4 +7,4 @@ set -e
 terraform init -backend-config="storage_account_name=$TF_STATE_STORAGE_ACCOUNT_NAME" \
     -backend-config="container_name=$TF_STATE_CONTAINER_NAME" \
     -backend-config="access_key=$(az keyvault secret show --name tfstate-storage-key --vault-name $KEYVAULT_NAME --query value -o tsv)" \
-    -backend-config="key=common.tfstate"
+    -backend-config="key=common.resources"
