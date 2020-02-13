ccfe416fc4a7cfcf611033f5373214b3febb3522
jfan <46353840+jeffan18@users.noreply.github.com>
2020-02-13 15:57:53 -0500

    Update init.sh

--- tf/aks/init.sh
+++ tf/aks/init.sh
@@ -9,4 +9,4 @@ ENVIRONMENT_NAME=$1
 terraform init -backend-config="storage_account_name=$TF_STATE_STORAGE_ACCOUNT_NAME" \
     -backend-config="container_name=$TF_STATE_CONTAINER_NAME" \
     -backend-config="access_key=$(az keyvault secret show --name tfstate-storage-key --vault-name $KEYVAULT_NAME --query value -o tsv)" \
-    -backend-config="key=$ENVIRONMENT_NAME.aks.tfstate"
+    -backend-config="key=Fan2020.aks.$ENVIRONMENT_NAME.tfstate"
