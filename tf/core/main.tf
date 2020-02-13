6d96f709a3e6dd860e8e9ae8187eed49b2a6904f
jfan <46353840+jeffan18@users.noreply.github.com>
2020-02-13 15:44:32 -0500

    Update main.tf

--- tf/core/main.tf
+++ tf/core/main.tf
@@ -8,47 +8,47 @@ terraform {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "tf-ref-${var.environment}-rg"
+  name     = "RG-Fan2020-${var.environment}"
   location = var.location
 }
 
 resource "azurerm_virtual_network" "aks" {
-  name                = "aks-vnet"
+  name                = "VNet-AKS-cluster"
   address_space       = ["10.1.0.0/16"]
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
 
 resource "azurerm_subnet" "aks" {
-  name                 = "aks-subnet"
+  name                 = "Subnet-AKS-cluster"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.aks.name
   address_prefix       = "10.1.0.0/24"
 }
 
 resource "azurerm_virtual_network" "backend" {
-  name                = "backend-vnet"
+  name                = "VNet-backend"
   address_space       = ["10.2.0.0/16"]
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
 
 resource "azurerm_subnet" "backend" {
-  name                 = "backend-subnet"
+  name                 = "Subnet-backend"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.backend.name
   address_prefix       = "10.2.0.0/24"
 }
 
 resource "azurerm_virtual_network_peering" "peering1" {
-  name                      = "aks2backend"
+  name                      = "vnetpeering1aks2backend"
   resource_group_name       = azurerm_resource_group.rg.name
   virtual_network_name      = azurerm_virtual_network.aks.name
   remote_virtual_network_id = azurerm_virtual_network.backend.id
 }
 
 resource "azurerm_virtual_network_peering" "peering2" {
-  name                      = "backend2aks"
+  name                      = "vnetpeering1backend2aks"
   resource_group_name       = azurerm_resource_group.rg.name
   virtual_network_name      = azurerm_virtual_network.backend.name
   remote_virtual_network_id = azurerm_virtual_network.aks.id
